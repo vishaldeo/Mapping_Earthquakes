4e5835cb1ab48d9865d6f2238589b69f14b40a46
Vishal Singh <vishal.singh@sony.com>
2022-11-21 23:45:11 -0800

    Earthquake 7 days.

--- Earthquakes_past7days/static/js/logic2.js
+++ Earthquakes_past7days/static/js/logic2.js
@@ -7,12 +7,12 @@ let torontoData = "https://raw.githubusercontent.com/vishaldeo/Mapping_Earthquak
 
 
 
-let dark = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+let satelliteStreets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 3,
   zoomOffset: -1,
-  id: "mapbox/dark-v10",
+  id: "mapbox/satellite-streets-v11",
   accessToken: API_KEY
 }) ;
 
@@ -27,39 +27,30 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  light: streets,
-  Dark: dark
+  "Streets": streets,
+  "Satellite": satelliteStreets
 };
 
 
 // Create the map object with a center and zoom level.
 let map = L.map('map', {
-  center: [30, 30],
+  center: [39.5, -98.5],
   zoom: 2,
   // collapsed: false,
   layers: [streets]
 });
 
-L.control.layers(baseMaps).addTo(map);
 
- // Our style object
-var mapStyle = {
-  color: "yellow",
-  fillColor: "pink",
-  fillOpacity: 0.5,
-  weight: 2
-};
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      // console.log(feature);
+      return L.marker(latlng)
+        .bindPopup("<h2> Earthquake Location : " + feature.properties.place + "</h2> <hr> <h3> Magnitude : " + feature.properties.mag + "</h3>" );
 
-// Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  // Passing in our style object
-  style: mapStyle,
-  onEachFeature: function(feature, layer) {
-       // Giving each feature a pop-up with information pertinent to it
-       layer.bindPopup("<h1> Airline : " + feature.properties.airline + "</h1> <hr> <h2> Destination:" + feature.properties.dst + "</h2>");
-
-  }
-}).addTo(map);
+    }
+  }).addTo(map);
 });
